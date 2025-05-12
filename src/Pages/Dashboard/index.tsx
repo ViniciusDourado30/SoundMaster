5461e6a505728a5f033d298562b4c6db1e1ff414
ViniciusDourado <viniciusdourado30@outlook.com>
2025-05-12 14:17:19 -0300

    Testar card de Album

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 const Dashboard = () => {
+    
     return(
-        <div className="h-full bg-[#121212]">
-            
+        <div className="h-full bg-[#121212] p-10">
+            <div className="w-28 h-28 rounded bg-cover"></div>;
         </div>
     )
 }
