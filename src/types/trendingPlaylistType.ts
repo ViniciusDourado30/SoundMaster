ecbbbd2d1cd1c4f80a1fc1c4a2e81db6de3c8d42
ViniciusDourado <viniciusdourado30@outlook.com>
2025-05-13 14:12:21 -0300

    Criar o tipo TrendingPlaylist

---
+++ src/types/trendingPlaylistType.ts
@@ -0,0 +1,10 @@
+export interface TrendingPlaylist{
+    id: string,
+    artwork: {
+        '150x150'?: string,
+        '480x480'?: string,  
+        '1000x1000'?: string,
+    }
+    description: string,
+    playlist_name: string,
+}
