3484bf6aa9cd50b1c3a3b3a028fca2ec3d71ef75
ViniciusDourado <viniciusdourado30@outlook.com>
2025-05-13 14:13:00 -0300

    Estilizar o AlbumCard

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -3,7 +3,7 @@ import AlbumCard from "./AlbumCard"
 const Dashboard = () => {    
     return(
         <div className="h-full bg-[#121212] p-10 flex">
-            <section className="w-full">
+            <section className="w-full h-fit overflow-x-scroll">
                 <label className="text-1xl font-bold uppercase text-[#eaeaea]">Trending</label>
                 <AlbumCard/>
             </section>
