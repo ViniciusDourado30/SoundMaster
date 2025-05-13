9540855b423e752fde0c9737a0d234ab51a1dd06
ViniciusDourado <viniciusdourado30@outlook.com>
2025-05-13 14:14:19 -0300

    Melhorar o consumo a API deixando o código mais limpo e legível

--- src/Pages/Dashboard/AlbumCard/index.tsx
+++ src/Pages/Dashboard/AlbumCard/index.tsx
@@ -1,12 +1,6 @@
 import { useEffect, useState } from "react";
-import getTrendingPlaylist from "../../../services/getTrendingPlaylist";
-
-interface TrendingPlaylist{
-    artwork: {
-        '1000x1000'?: string,
-    }
-    description: string,
-};
+import { PlaylistService } from "../../../services/playlistService";
+import type { TrendingPlaylist } from "../../../types/trendingPlaylistType";
 
 const AlbumCard = () => {
 
@@ -14,7 +8,7 @@ const AlbumCard = () => {
 
       useEffect(() => {
         const getData = async () => {
-            const data = await getTrendingPlaylist();
+            const data = await PlaylistService.getTrendingPlaylist();
             setTrendingPlaylist(data);
             console.log(data)
         };
@@ -22,7 +16,13 @@ const AlbumCard = () => {
     }, []);
 
     return(
-    <div className="w-36 h-36 rounded bg-cover bg-red-500" style={{ backgroundImage: `url(${trendingPlaylist?.artwork["1000x1000"]})` }}></div>
+    <div className="w-36 min-w-36 h-36 min-h-36 rounded-xl bg-cover border-2 border-[#191919] cursor-pointer relative overflow-hidden" style={{ backgroundImage: `url(${trendingPlaylist?.artwork["1000x1000"]  })` }}>
+        <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+        <div className="relative w-full h-full flex justify-end p-3 flex-col">
+            <h1 className="text-white font-medium">{trendingPlaylist?.playlist_name}</h1>
+            <p className="text-white text-sm font-light line-clamp-1 hover:line-clamp-none">{trendingPlaylist?.description}</p>
+        </div>
+    </div>
     )
 }
 
