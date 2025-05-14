79941a14676d8459e19fb5c8e62e3aa8ffa9e46b
ViniciusDourado <viniciusdourado30@outlook.com>
2025-05-14 00:33:29 -0300

    Corrigir erros no funinamento da scrollbar de playlists

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -3,9 +3,10 @@ import AlbumCard from "./AlbumCard"
 const Dashboard = () => {    
     return(
         <div className="h-full bg-[#121212] p-10 flex">
-            <section className="w-full h-fit overflow-x-scroll">
-                <label className="text-1xl font-bold uppercase text-[#eaeaea]">Trending</label>
-                <AlbumCard/>
+            <section className="w-full h-fit">
+                <label className="static text-1xl font-bold uppercase text-[#eaeaea]">Trending</label>
+                <div className="w-full h-fit flex gap-3 overflow-x-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-transparent">
+                    <AlbumCard/></div>
             </section>
         </div>
     )
