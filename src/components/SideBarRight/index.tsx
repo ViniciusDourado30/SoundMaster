d3b53f60491ac244e3eb72d99ab358f2c4527d95
Vinicius <viniciusdourado30@outlook.com>
2025-05-17 04:16:29 -0300

    Criar o componente SideBarRight

---
+++ src/components/SideBarRight/index.tsx
@@ -0,0 +1,9 @@
+const SideBarRight = () => {
+    return(
+        <div className="w-80 h-full bg-[#0A0A0A] flex flex-col justify-between items-center">
+            
+        </div>
+    )
+}
+
+export default SideBarRight
