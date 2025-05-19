37cf1d23bd74250683e626d0289c98c09fbb54b7
Vinicius <viniciusdourado30@outlook.com>
2025-05-19 02:05:32 -0300

    Mudar propriedade do tailwind

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,6 +1,6 @@
 const SideBar = () => {
     return(
-        <div className="w-28 h-screen bg-[#0A0A0A] flex flex-col justify-between items-center">
+        <div className="w-28 h-full bg-[#0A0A0A] flex flex-col justify-between items-center py-5">
             <img src="/src/assets/images/Logo.png" alt="Logo SoundMaster" className="p-3"/>
         </div>
     )
