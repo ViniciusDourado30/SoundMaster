2d7483f5ab9e6b13affdd7bc1477ef853ebc4822
Vinicius <viniciusdourado30@outlook.com>
2025-05-19 02:04:59 -0300

    Fazer a requisição correta

--- src/services/trackServices.tsx
+++ src/services/trackServices.tsx
@@ -1,13 +1,27 @@
+
+import type { playlistContent } from "../types/playlistContent";
 import discoveryProviderAudiusAPI from "./api/axiosInstanceDiscoveryProviderAudius";
 
 export const TrackService = {
-    getTrack: async () => {
+    getTracksByPlaylist: async (tracksIds: playlistContent[]) => {
         try {
-            const response = await discoveryProviderAudiusAPI.get('/tracks/pz85lmX');
-            console.log(response.data.data)
-            return response.data.data;
+            const response = await Promise.all(
+                tracksIds.map(async (track) => {
+                    const data = await discoveryProviderAudiusAPI.get(`/tracks/${track.track_id}`);
+                    console.log(data);
+                     return {
+      id: data.data.data.id,
+      title: data.data.data.title,
+      description: data.data.data.description,
+      artwork: data.data.data.artwork,
+      name: data.data.data.user.name,
+    };
+                })
+            )
+            return (response);
         } catch (error) {
             console.log(error);
+            return [];
         }
     }
 }
